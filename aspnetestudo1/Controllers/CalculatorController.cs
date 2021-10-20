f6ab9b48ec22644990b13116843248ea7b7a6b3d
alemaxi <alelogovir@gmail.com>
2021-10-20 18:07:34 -0300

    funcao soma adicionada

---
+++ aspnetestudo1/Controllers/CalculatorController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace aspnetestudo1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Get(string firstNumber, string secondNumber)
+        {
+            if(IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return Ok(sum);
+            }
+            return BadRequest("input is invalid");
+        }
+
+        private decimal ConvertToDecimal(string convertNumber)
+        {
+            decimal value;
+            if(decimal.TryParse(convertNumber, out value))
+            {
+                return value;
+            }
+            return 0;
+        }
+
+        private bool IsNumeric(string convertNumber)
+        {
+            double number;
+            bool isNumber = double.TryParse(convertNumber, System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo, out number);
+            return isNumber;
+        }
+    }
+}
